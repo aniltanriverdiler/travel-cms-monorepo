953c1f096c22c7b95e81dfa0a0109cd14d956f63
Anıl Tanrıverdiler <186810775+aniltanriverdiler@users.noreply.github.com>
2025-10-11 22:00:39 +0300

    feat(travel): add Hero section component

--- travel/src/app/(routes)/(home)/page.tsx
+++ travel/src/app/(routes)/(home)/page.tsx
@@ -1,9 +1,11 @@
 import RecentProduct from "@/components/layout/recent/RecentProduct";
 import React from "react";
+import Hero from "./_components/Hero/Hero";
 
 function Home() {
   return (
     <div>
+      <Hero />
       <RecentProduct />
     </div>
   );

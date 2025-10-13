e6bf48012040f2b74d5295956d9c4c64051073f8
Anıl Tanrıverdiler <186810775+aniltanriverdiler@users.noreply.github.com>
2025-10-13 18:42:17 +0300

    feat(travel): add scrolling brand logo animation using react-fast-marquee

--- travel/src/app/(routes)/about/page.tsx
+++ travel/src/app/(routes)/about/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutUs from "./_components/AboutUs";
 import TravelStats from "./_components/TravelStats";
+import CompanyLogos from "./_components/CompanyLogos";
 
 function AboutPage() {
   return (
     <div>
       <AboutUs />
       <TravelStats />
+      <CompanyLogos />
     </div>
   );
 }
